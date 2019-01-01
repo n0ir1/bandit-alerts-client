f6fd8d33c9ed9e41cee02af9cf206f1aef84855a
n0ir <smile955@inbox.ru>
2019-01-02 00:34:43 +0300

    cookie

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 import cookies from "js-cookie";
+import cookie from "cookie";
 
 export const setToken = token => {
   cookies.set("token", token);
@@ -8,4 +9,11 @@ export const setToken = token => {
 export const removeToken = () => {
   cookies.remove("token");
   location.reload();
+};
+
+export const parseCookies = (req, options = {}) => {
+  return cookie.parse(
+    req ? req.headers.cookie || "" : document.cookie,
+    options
+  );
 };
